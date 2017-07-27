24d34c6d61bc217c89db4c9752401686c0ec3410
Cyril PATRICE <cpatrice@pagesjaunes.fr>
2017-07-27 10:41:23 +0200

    feat(extraction): debug mode

--- server.js
+++ server.js
@@ -6,19 +6,58 @@ var cache = require('memory-cache');
 
 app.set('port', (process.env.PORT || 5000));
 var populateCache = function(json) {
+  json.versionMin="8.5";
   cache.put(json.appId, json, 1000 * 86400, function(key, value) {
     console.log(value);
   });
   console.log(json.version);
 };
+
+//TODO set elems from query 
+var populateCacheForDebug = function(json, key, params) {
+  json.appId=key;
+  json.version = params.version || "9.0.0";
+  console.log(params);
+  console.log("HERE");
+  json.versionMin = params.version_min || "8.4.1.0";
+//  console.log(json)
+  cache.put(json.appId, json, 1000, function(key, value) {
+    console.log(value);
+  });
+  console.log(json.version);
+};
+
+var isDebugMode = function(appId) {
+  if(appId.indexOf(".latest") !== -1 ||
+    appId.indexOf(".delivery") !== -1 ||
+    appId.indexOf(".debug") !== -1)
+    return true;
+
+  return false;
+}
+var basePackage = function(appId){
+  return  appId.replace(/\.(latest|delivery|beta)/,""); 
+}
+
 app.get('/p/:package', function(req, res){
   var key = req.params.package;
+  
   //All the web scraping magic will happen here
   console.log(key);
   if(!cache.get(key))
   {
-    gplay.app({appId: req.params.package})
-    .then(populateCache, console.log).then((result) => {res.json(cache.get(key)); console.log("failed")})       
+    if (isDebugMode(req.params.package)){
+      gplay.app({appId: basePackage(key), lang:'fr'})
+      .then((app) => populateCacheForDebug(app, key, req.query), console.log).then((result) => {
+        res.json(cache.get(key));
+        console.log("debug added")
+      }) 
+    } else    
+    gplay.app({appId: key})
+    .then(populateCache, console.log).then((result) => {
+      res.json(cache.get(key));
+      console.log(" added")
+    })       
   } else
   {
     console.log("cached "+ cache.keys())
